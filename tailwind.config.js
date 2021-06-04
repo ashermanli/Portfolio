e624ad22e165cfa76a073932cafbc07818f6f287
ashermanli <ashermanli@gmail.com>
2021-06-03 18:13:30 -0600

    added dev script and updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: [
-    './src/**/*/.html',
-    './src/**/*/.js'
-  ],
+  
+  purge:{
+    content: ['./src/**/*/.html','./src/**/*/.js'],
+    options:{
+      safeList: ['bg-matte-black']
+    }
+  } ,
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Nunito': ['Nunito', ...defaultTheme.fontFamily.sans]
+      }, 
+      colors: {
+        black: colors.black,
+        gray: colors.trueGray,
+        indigo: colors.indigo,
+        cyan: colors.cyan
+      }
+    },
   },
   variants: {
     extend: {},
