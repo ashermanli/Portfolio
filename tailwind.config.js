a07ea402eff5e1b88e9d79bb6c0e298cfc48ba31
ashermanli <ashermanli@gmail.com>
2021-06-11 17:32:31 -0600

    updated navbar links and project card styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 module.exports = {
-  
+  mode:'jit',
   purge:['./**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
